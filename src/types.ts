b5d331ec45e5f81dc5521f8344e303f06715c5d5
redhoodsu <redhoodsu@tencent.com>
2025-02-24 19:08:11 +0800

    chore: add eslint

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,7 @@
 import { TcpNetConnectOpts } from 'net'
 
-export interface ClientOptions extends TcpNetConnectOpts {}
+export interface ClientOptions extends TcpNetConnectOpts {
+  bin?: string
+}
 
 export type Parameters = Record<string, string>
