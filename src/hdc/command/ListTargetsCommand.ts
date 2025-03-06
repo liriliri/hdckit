9341eb321dd96c5b44dc5494b234ab0da31e8ada
redhoodsu <redhoodsu@tencent.com>
2025-03-06 13:29:21 +0800

    release: v0.3.4

--- src/hdc/command/ListTargetsCommand.ts
+++ src/hdc/command/ListTargetsCommand.ts
@@ -1,13 +1,16 @@
 import Command from '../Command'
+import contain from 'licia/contain'
 
 export default class ListTargetsCommand extends Command<string[]> {
   async execute() {
     this.send('list targets')
     return this.connection.readValue().then((data) => {
-      return data
-        .toString()
-        .split('\n')
-        .filter((line) => line)
+      const result = data.toString()
+      if (contain(result, 'Empty')) {
+        return []
+      }
+
+      return result.split('\n').filter((line) => line)
     })
   }
 }
